b5801d71e64fd6b08be6ecc30b911ae29a97627c
liu.chong <liu.chong@motor-fun.com>
2021-03-28 21:26:01 +0800

    优化代码

--- core/fo/fo.go
+++ core/fo/fo.go
@@ -7,52 +7,45 @@ import (
 	"gopkg.in/ini.v1"
 )
 
+func LoadConf() *ini.File {
+	cfg, err := ini.Load("conf.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return cfg
+}
+
 func GetTday() string {
 	td := time.Now().Format("2006-01-02")
 	return td
 }
 
 func GetMainPath() string {
-	cfg, err := ini.Load("conf.ini")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	cfg := LoadConf()
 	lmp := cfg.Section("").Key("logmainpath").String()
 	return lmp
 }
 
 func GetFileName() string {
-	cfg, err := ini.Load("conf.ini")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	cfg := LoadConf()
 	fn := cfg.Section("").Key("filename").String()
 	return fn
 }
 
 func GetApi(conf string) string {
-	cfg, err := ini.Load(conf)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	cfg := LoadConf()
 	ddapi := cfg.Section("apis").Key("ddapi").String()
 	return ddapi
 }
 
 func GetLog(conf string) string {
-	cfg, err := ini.Load(conf)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	cfg := LoadConf()
 	logpath := cfg.Section("paths").Key("logpath").String()
 	return logpath
 }
 
 func GetMsg(conf string) string {
-	cfg, err := ini.Load(conf)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	cfg := LoadConf()
 	reg_str := cfg.Section("data").Key("reg_str").String()
 	return reg_str
 }
