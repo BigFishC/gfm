a61878826756ff963bbcebd0e84fc516f3cd64d4
BigFishC <liuchong_4589@163.com>
2021-03-29 18:01:42 +0800

    增加README

---
+++ README.md
@@ -0,0 +1,7 @@
+# GFM
+
+## Golang File Monitor
+- GFM是一个简单开源的文件监控系统
+- 支持Windows、Linux操作系统
+- 简单易配置
+- 支持钉钉告警
--- core/rsinit/rsinit.go
+++ core/rsinit/rsinit.go
@@ -1,15 +1,24 @@
 package rsinit
 
 import (
+	"log"
+	"os"
+
 	"github.com/gfm/core/fo"
 )
 
-func InitData() {
+func InitConfData() {
 	cfg := fo.LoadConf()
+	cfg.Section("").Key("filename").SetValue("error-" + fo.GetTday() + ".log")
+	cfg.SaveTo(fo.ConfContent.ConfName)
 	cfg.Section("paths").Key("logpath").SetValue(fo.GetMainPath() + fo.GetTday() + "/" + fo.GetFileName())
 	cfg.SaveTo(fo.ConfContent.ConfName)
+	_, err := os.Stat(fo.GetLog(fo.ConfContent.ConfName))
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func RunInit() {
-	InitData()
+	InitConfData()
 }
