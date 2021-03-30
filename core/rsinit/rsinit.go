f1a58b39f22c107d87df17aac5349d1fb3506836
BigFishC <liuchong_4589@163.com>
2021-03-30 10:37:24 +0800

    add windows feature

--- core/rsinit/rsinit.go
+++ core/rsinit/rsinit.go
@@ -3,6 +3,7 @@ package rsinit
 import (
 	"log"
 	"os"
+	"runtime"
 
 	"github.com/gfm/core/fo"
 )
@@ -11,8 +12,16 @@ func InitConfData() {
 	cfg := fo.LoadConf()
 	cfg.Section("").Key("filename").SetValue("error-" + fo.GetTday() + ".log")
 	cfg.SaveTo(fo.ConfContent.ConfName)
-	cfg.Section("paths").Key("logpath").SetValue(fo.GetMainPath() + fo.GetTday() + "/" + fo.GetFileName())
-	cfg.SaveTo(fo.ConfContent.ConfName)
+
+	if runtime.GOOS == "linux" {
+		cfg.Section("paths").Key("logpath").SetValue(fo.GetMainPath() + fo.GetTday() + "/" + fo.GetFileName())
+		cfg.SaveTo(fo.ConfContent.ConfName)
+	} else if runtime.GOOS == "windows" {
+		cfg.Section("paths").Key("logpath").SetValue(fo.GetMainPath() + "\\" + "\\" + fo.GetTday() + "\\" + "\\" + fo.GetFileName())
+		cfg.SaveTo(fo.ConfContent.ConfName)
+	} else {
+		log.Fatalln("OS 操作系统不可用！")
+	}
 	_, err := os.Stat(fo.GetLog(fo.ConfContent.ConfName))
 	if err != nil {
 		log.Fatalln(err)
